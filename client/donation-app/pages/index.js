294c87c9ba93a65cbca027270c2371c9884c7464
jessicreative <jjc2295@columbia.edu>
2023-04-15 17:30:00 -0400

    Can list all relevant information on frontend

--- client/donation-app/pages/index.js
+++ client/donation-app/pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
   const callAPI = async () => {
 		try {
 			const res = await fetch(
-        `https://api.endaoment.org/v1/sdk/funds`
+        `https://api.endaoment.org/v1/sdk/orgs`
 			);
 			const data = await res.json();
 			console.log(data);
@@ -27,82 +27,47 @@ export default function Home() {
           <ConnectWallet />
         </div>
         <button onClick={callAPI}>Make API call</button>
-        {names.map((name) => (
-          <li key={name.id}>{name.name}</li>
-        ))}
         <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
             <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
-                        <th scope="col" class="px-6 py-3">
-                            Product name
+                        <th scope="col" className="px-6 py-3">
+                            Name
                         </th>
-                        <th scope="col" class="px-6 py-3">
-                            Color
+                        <th scope="col" className="px-6 py-3">
+                            Contract Address
                         </th>
-                        <th scope="col" class="px-6 py-3">
-                            Category
+                        <th scope="col" className="px-6 py-3">
+                            ein
                         </th>
-                        <th scope="col" class="px-6 py-3">
-                            Price
+                        <th scope="col" className="px-6 py-3">
+                            Description
                         </th>
-                        <th scope="col" class="px-6 py-3">
+                        <th scope="col" className="px-6 py-3">
                             Action
                         </th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Apple MacBook Pro 17"
-                        </th>
-                        <td class="px-6 py-4">
-                            Silver
-                        </td>
-                        <td class="px-6 py-4">
-                            Laptop
-                        </td>
-                        <td class="px-6 py-4">
-                            $2999
-                        </td>
-                        <td class="px-6 py-4">
-                            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                        </td>
-                    </tr>
-                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Microsoft Surface Pro
-                        </th>
-                        <td class="px-6 py-4">
-                            White
-                        </td>
-                        <td class="px-6 py-4">
-                            Laptop PC
-                        </td>
-                        <td class="px-6 py-4">
-                            $1999
-                        </td>
-                        <td class="px-6 py-4">
-                            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                        </td>
-                    </tr>
-                    <tr class="bg-white dark:bg-gray-800">
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Magic Mouse 2
-                        </th>
-                        <td class="px-6 py-4">
-                            Black
-                        </td>
-                        <td class="px-6 py-4">
-                            Accessories
-                        </td>
-                        <td class="px-6 py-4">
-                            $99
-                        </td>
-                        <td class="px-6 py-4">
-                            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                        </td>
-                    </tr>
+                  {names.map((name) => (
+                    <tr key={name.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                          <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {name.name}
+                          </th>
+                          <td className="px-6 py-4">
+                            {name.contractAddress}
+                          </td>
+                          <td className="px-6 py-4">
+                            {name.ein}
+                          </td>
+                          <td className="px-6 py-4">
+                            {name.nteeDescription}
+                          </td>
+                          <td className="px-6 py-4">
+                              <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Donate</a>
+                          </td>
+                      </tr>
+                  ))}
                 </tbody>
             </table>
         </div>
