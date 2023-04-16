ad541a7e50cfbac90c760a8ec076ecd0a594b381
jessicreative <jjc2295@columbia.edu>
2023-04-15 20:45:23 -0400

    Fixed some ui stuff

--- client/donation-app/pages/orgs.js
+++ client/donation-app/pages/orgs.js
@@ -18,7 +18,12 @@ export default function Orgs() {
 
     return (
         <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-            <button onClick={callAPI}>Make API call</button>
+            <center>
+                <button onClick={callAPI} type="button" 
+                class="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                    Get Organizations
+                </button>
+            </center>
             <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
--- client/donation-app/pages/stream.js
+++ client/donation-app/pages/stream.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import styles from "../styles/Home.module.css";
 import { Framework } from "@superfluid-finance/sdk-core";
 import {
   Button,
@@ -10,63 +11,63 @@ import {
 } from "react-bootstrap";
 import { ethers } from "ethers";
 
-export default function Home() {
+export default function Stream() {
 
-  let account;
+//   let account;
 
-  //where the Superfluid logic takes place
-  async function createNewFlow(recipient, flowRate) {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    await provider.send("eth_requestAccounts", []);
+//   //where the Superfluid logic takes place
+//   async function createNewFlow(recipient, flowRate) {
+//     const provider = new ethers.providers.Web3Provider(window.ethereum);
+//     await provider.send("eth_requestAccounts", []);
 
-    const signer = provider.getSigner();
+//     const signer = provider.getSigner();
 
-    const chainId = await window.ethereum.request({ method: "eth_chainId" });
-    const sf = await Framework.create({
-      chainId: Number(chainId),
-      provider: provider
-    });
+//     const chainId = await window.ethereum.request({ method: "eth_chainId" });
+//     const sf = await Framework.create({
+//       chainId: Number(chainId),
+//       provider: provider
+//     });
 
-    const superSigner = sf.createSigner({ signer: signer });
+//     const superSigner = sf.createSigner({ signer: signer });
 
-    console.log(signer);
-    console.log(await superSigner.getAddress());
-    const daix = await sf.loadSuperToken("fDAIx");
+//     console.log(signer);
+//     console.log(await superSigner.getAddress());
+//     const daix = await sf.loadSuperToken("fDAIx");
 
-    console.log(daix);
+//     console.log(daix);
 
-    try {
-      const createFlowOperation = daix.createFlow({
-        sender: await superSigner.getAddress(),
-        receiver: recipient,
-        flowRate: flowRate
-        // userData?: string
-      });
+//     try {
+//       const createFlowOperation = daix.createFlow({
+//         sender: await superSigner.getAddress(),
+//         receiver: recipient,
+//         flowRate: flowRate
+//         // userData?: string
+//       });
 
-      console.log(createFlowOperation);
-      console.log("Creating your stream...");
+//       console.log(createFlowOperation);
+//       console.log("Creating your stream...");
 
-      const result = await createFlowOperation.exec(superSigner);
-      console.log(result);
+//       const result = await createFlowOperation.exec(superSigner);
+//       console.log(result);
 
-      console.log(
-        `Congrats - you've just created a money stream!
-      `
-      );
-    } catch (error) {
-      console.log(
-        "Hmmm, your transaction threw an error. Make sure that this stream does not already exist, and that you've entered a valid Ethereum address!"
-      );
-      console.error(error);
-    }
-  }
+//       console.log(
+//         `Congrats - you've just created a money stream!
+//       `
+//       );
+//     } catch (error) {
+//       console.log(
+//         "Hmmm, your transaction threw an error. Make sure that this stream does not already exist, and that you've entered a valid Ethereum address!"
+//       );
+//       console.error(error);
+//     }
+//   }
 
-  // export const CreateFlow = () => {
-    const CreateFlow = () => {
-    const [recipient, setRecipient] = useState("");
-    const [isButtonLoading, setIsButtonLoading] = useState(false);
-    const [flowRate, setFlowRate] = useState("");
-    const [flowRateDisplay, setFlowRateDisplay] = useState("");
+//   // export const CreateFlow = () => {
+//     const CreateFlow = () => {
+//     const [recipient, setRecipient] = useState("");
+//     const [isButtonLoading, setIsButtonLoading] = useState(false);
+//     const [flowRate, setFlowRate] = useState("");
+//     const [flowRateDisplay, setFlowRateDisplay] = useState("");
     const [currentAccount, setCurrentAccount] = useState("");
 
     const connectWallet = async () => {
@@ -91,128 +92,133 @@ export default function Home() {
       }
     };
 
-    const checkIfWalletIsConnected = async () => {
-      console.log("runs");
-      const { ethereum } = window;
+//     const checkIfWalletIsConnected = async () => {
+//       console.log("runs");
+//       const { ethereum } = window;
 
-      if (!ethereum) {
-        console.log("Make sure you have metamask!");
-        return;
-      } else {
-        console.log("We have the ethereum object", ethereum);
-      }
+//       if (!ethereum) {
+//         console.log("Make sure you have metamask!");
+//         return;
+//       } else {
+//         console.log("We have the ethereum object", ethereum);
+//       }
 
-      const accounts = await window.ethereum.request({ method: "eth_accounts" });
-      const chain = await window.ethereum.request({ method: "eth_chainId" });
-      let chainId = chain;
-      console.log("chain ID:", chain);
-      console.log("global Chain Id:", chainId);
-      if (accounts.length !== 0) {
-        account = accounts[0];
-        console.log("Found an authorized account:", account);
-        setCurrentAccount(account);
-        // Setup listener! This is for the case where a user comes to our site
-        // and ALREADY had their wallet connected + authorized.
-        // setupEventListener()
-      } else {
-        console.log("No authorized account found");
-      }
-    };
+//       const accounts = await window.ethereum.request({ method: "eth_accounts" });
+//       const chain = await window.ethereum.request({ method: "eth_chainId" });
+//       let chainId = chain;
+//       console.log("chain ID:", chain);
+//       console.log("global Chain Id:", chainId);
+//       if (accounts.length !== 0) {
+//         account = accounts[0];
+//         console.log("Found an authorized account:", account);
+//         setCurrentAccount(account);
+//         // Setup listener! This is for the case where a user comes to our site
+//         // and ALREADY had their wallet connected + authorized.
+//         // setupEventListener()
+//       } else {
+//         console.log("No authorized account found");
+//       }
+//     };
 
-    useEffect(() => {
-      checkIfWalletIsConnected();
-    }, []);
+//     useEffect(() => {
+//       checkIfWalletIsConnected();
+//     }, []);
 
-    function calculateFlowRate(amount) {
-      if (typeof Number(amount) !== "number" || isNaN(Number(amount)) === true) {
-        alert("You can only calculate a flowRate based on a number");
-        return;
-      } else if (typeof Number(amount) === "number") {
-        if (Number(amount) === 0) {
-          return 0;
-        }
-        const amountInWei = ethers.BigNumber.from(amount);
-        const monthlyAmount = ethers.utils.formatEther(amountInWei.toString());
-        const calculatedFlowRate = monthlyAmount * 3600 * 24 * 30;
-        return calculatedFlowRate;
-      }
-    }
+//     function calculateFlowRate(amount) {
+//       if (typeof Number(amount) !== "number" || isNaN(Number(amount)) === true) {
+//         alert("You can only calculate a flowRate based on a number");
+//         return;
+//       } else if (typeof Number(amount) === "number") {
+//         if (Number(amount) === 0) {
+//           return 0;
+//         }
+//         const amountInWei = ethers.BigNumber.from(amount);
+//         const monthlyAmount = ethers.utils.formatEther(amountInWei.toString());
+//         const calculatedFlowRate = monthlyAmount * 3600 * 24 * 30;
+//         return calculatedFlowRate;
+//       }
+//     }
 
-    function CreateButton({ isLoading, children, ...props }) {
-      return (
-        <Button variant="success" className="button" {...props}>
-          {isButtonLoading ? <Spinner animation="border" /> : children}
-        </Button>
-      );
-    }
+//     function CreateButton({ isLoading, children, ...props }) {
+//       return (
+//         <Button variant="success" className="button" {...props}>
+//           {isButtonLoading ? <Spinner animation="border" /> : children}
+//         </Button>
+//       );
+//     }
 
-    const handleRecipientChange = (e) => {
-      setRecipient(() => ([e.target.name] = e.target.value));
-    };
+//     const handleRecipientChange = (e) => {
+//       setRecipient(() => ([e.target.name] = e.target.value));
+//     };
 
-    const handleFlowRateChange = (e) => {
-      setFlowRate(() => ([e.target.name] = e.target.value));
-      let newFlowRateDisplay = calculateFlowRate(e.target.value);
-      setFlowRateDisplay(newFlowRateDisplay.toString());
-    };
+//     const handleFlowRateChange = (e) => {
+//       setFlowRate(() => ([e.target.name] = e.target.value));
+//       let newFlowRateDisplay = calculateFlowRate(e.target.value);
+//       setFlowRateDisplay(newFlowRateDisplay.toString());
+//     };
 
     return (
-      <div>
-        <h2>Create a Flow</h2>
-        {currentAccount === "" ? (
-          <button id="connectWallet" className="button" onClick={connectWallet}>
-            Connect Wallet
-          </button>
-        ) : (
-          <Card>
-            {`${currentAccount.substring(0, 4)}...${currentAccount.substring(
-              38
-            )}`}
-          </Card>
-        )}
-        <Form>
-          <FormGroup className="mb-3">
-            <FormControl
-              name="recipient"
-              value={recipient}
-              onChange={handleRecipientChange}
-              placeholder="Enter recipient address"
-            ></FormControl>
-          </FormGroup>
-          <FormGroup className="mb-3">
-            <FormControl
-              name="flowRate"
-              value={flowRate}
-              onChange={handleFlowRateChange}
-              placeholder="Enter a flowRate in wei/second"
-            ></FormControl>
-          </FormGroup>
-          <CreateButton
-            onClick={() => {
-              setIsButtonLoading(true);
-              createNewFlow(recipient, flowRate);
-              setTimeout(() => {
-                setIsButtonLoading(false);
-              }, 1000);
-            }}
-          >
-            Click to Create Your Stream
-          </CreateButton>
-        </Form>
-
         <div>
-          <p>
-            Go to the CreateFlow.js component and look at the <b>createFlow() </b>
-            function to see under the hood
-          </p>
-          <div>
-            <p>Your flow will be equal to:</p>
-            <p>
-              <b>${flowRateDisplay !== " " ? flowRateDisplay : 0}</b> DAIx/month
-            </p>
-          </div>
+            <h2 className = "mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Create a Flow</h2>
+            {currentAccount === "" ? (
+                <center>
+                    <button id="connectWallet" onClick={connectWallet} type="button" 
+                    class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium 
+                    rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+                        Connect Wallet
+                    </button>
+                </center>
+            ) : (
+            <Card>
+                {`${currentAccount.substring(0, 4)}...${currentAccount.substring(
+                38
+                )}`}
+            </Card>
+            )}
         </div>
-      </div>
+//         <Form>
+//           <FormGroup className="mb-3">
+//             <FormControl
+//               name="recipient"
+//               value={recipient}
+//               onChange={handleRecipientChange}
+//               placeholder="Enter recipient address"
+//             ></FormControl>
+//           </FormGroup>
+//           <FormGroup className="mb-3">
+//             <FormControl
+//               name="flowRate"
+//               value={flowRate}
+//               onChange={handleFlowRateChange}
+//               placeholder="Enter a flowRate in wei/second"
+//             ></FormControl>
+//           </FormGroup>
+//           <CreateButton
+//             onClick={() => {
+//               setIsButtonLoading(true);
+//               createNewFlow(recipient, flowRate);
+//               setTimeout(() => {
+//                 setIsButtonLoading(false);
+//               }, 1000);
+//             }}
+//           >
+//             Click to Create Your Stream
+//           </CreateButton>
+//         </Form>
+
+//         <div>
+//           <p>
+//             Go to the CreateFlow.js component and look at the <b>createFlow() </b>
+//             function to see under the hood
+//           </p>
+//           <div>
+//             <p>Your flow will be equal to:</p>
+//             <p>
+//               <b>${flowRateDisplay !== " " ? flowRateDisplay : 0}</b> DAIx/month
+//             </p>
+//           </div>
+//         </div>
+//       </div>
     );
-  };
+//   };
 }
